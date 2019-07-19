1f86597d81d776f130d89f9c726c464fa2fd3150
Ashley Broadley <ashley.broadley@skybettingandgaming.com>
2019-07-19 10:17:55 +0000

    Better solution to granting permission to the docker socket. Also replacing all tabs in the entrypoint with spaces, because spaces \> tabs.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 # Git config
 if [ ! -z "$GIT_USER_NAME" ] && [ ! -z "$GIT_USER_EMAIL" ]; then
-	git config --global user.name "$GIT_USER_NAME"
-	git config --global user.email "$GIT_USER_EMAIL"
+    git config --global user.name "$GIT_USER_NAME"
+    git config --global user.email "$GIT_USER_EMAIL"
 fi
 
 # Get standard cali USER_ID variable
@@ -20,7 +20,14 @@ if [ ! -z "$USER_ID" ] && [ "$(id -u me)" != "$USER_ID" ]; then
 fi
 chown -R me: /home/me
 
-# Setting permissions on docker.sock
-chgrp group /var/run/docker.sock
+# Give user `me` permission to use docker!
+if [ -f "/var/run/docker.sock" ]; then
+    # Find the hosts group ID for the docker socket
+    HOST_DOCKER_SOCKET_GROUP_ID=`stat -c %g /var/run/docker.sock`
+    # create the group `docker`
+    groupadd --non-unique -g "$HOST_DOCKER_SOCKET_GROUP_ID" docker
+    # add `me` to the `docker` group
+    adduser me docker
+fi
 
 exec /sbin/su-exec me tmux -u -2 new -s ${PROJECT_NAME}
